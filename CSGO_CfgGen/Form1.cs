e0ef36821fd4f90f186a06e78788c916557d3af2
DieQuelle <Die_Quelle88@gmx.de>
2014-09-19 18:19:56 +0200

    test Form1.cs

--- CSGO_CfgGen/Form1.cs
+++ CSGO_CfgGen/Form1.cs
@@ -63,6 +63,8 @@ namespace CSGO_CfgGen
         {
             this.cfgFileManager = cfgFileMan;
             InitializeComponent();
+
+            // TEST
             InitializeCustomComponent();
         }
 
