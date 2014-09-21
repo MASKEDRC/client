a12596d4df6932663906d1b7705b51150b09a99f
DieQuelle <Die_Quelle88@gmx.de>
2014-09-21 18:57:28 +0200

    Keys aktuallisiert

--- CSGO_CfgGen/KeyToken.cs
+++ CSGO_CfgGen/KeyToken.cs
@@ -7,26 +7,41 @@ namespace CSGO_CfgGen.Commands
 {
     public enum KeyToken
     {
-        //Andere
-        Other,
+        //Letter Keys
+        A        B        C        D        E        F        G        H        I        J        K        L        M        N        O        P        Q        R        S        T        U        V        W        X        Y        Z        //Number Keys        1        2        3        4        5        6        7        8        9        0                //Function Keys        F1        F2        F3        F4        F5        F6        F7        F8        F9        F10        F11        F12        //Arrow Keys        LEFTARROW        RIGHTARROW        UPARROW        DOWNARROW        //Special Keys        ENTER        SPACE        SHIFT        CTRL        PAUSE        ALT        BACKSPACE        TAB        CAPSLOCK                //Scroll Navigation Keys        INS        DEL        HOME        END        PGUP        PGDN
 
-        //Keypad
-        KP_SLASH,
-        KP_DEL,
-        KP_PLUS,
-        KP_MINUS,
-        KP_MULTIPLY,
-        KP_ENTER,
-        KP_END,
-        KP_DOWNARROW,
-        KP_PGDN,
-        KP_LEFTARROW,
-        KP_5,
-        KP_RIGHTARROW,
-        KP_HOME,
-        KP_UPARROW,
-        KP_PGUP,
-        KP_INS
 
+        //Numpad
+
+        KP_MULTIPLY                     //- "*"
+        KP_MINUS                        //- "-" 
+        KP_PLUS                         //- "+"
+        KP_INS                          //- 0 / Ins
+        KP_DEL                          //- "." / Del
+        KP_END                          //- 1 / End
+        KP_DOWNARROW                    //- 2 / Down Arrow
+        KP_PGDN                         //- 3 / Page Down
+        KP_LEFTARROW                    //- 4 / Left Arrow
+        KP_5                            //- 5 (Centre Button)  
+        KP_RIGHTARROW                   //- 6 / Right Arrow
+        KP_HOME                         //- 7 / Home
+        KP_UPARROW                      //- 8 / Up Arrow
+        KP_PGUP                         //- 9 / Page Up
+
+        
+        //Misc Keys  (later) (depends on Keyboard Layout....)
+        /*
+ 
+     * ,
+     * .
+     * /
+     * \
+     * ;
+     * '
+     * *
+     * [
+
+        */
+        
     }
 }
