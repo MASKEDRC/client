577e17150419eeee02a915dcd50fe21d38b41751
monoxacc@gmail.com <monoxacc@gmail.com>
2014-09-21 21:49:26 +0200

    KeyToken(Tino)

--- CSGO_CfgGen/KeyToken.cs
+++ CSGO_CfgGen/KeyToken.cs
@@ -7,8 +7,68 @@ namespace CSGO_CfgGen.Commands
 {
     public enum KeyToken
     {
-        //Andere
-        Other,
+        //Default
+        UNKNOWN = 0,
+
+        //Number keys
+        KEY_0,
+        KEY_1,
+        KEY_2,
+        KEY_3,
+        KEY_4,
+        KEY_5,
+        KEY_6,
+        KEY_7,
+        KEY_8,
+        KEY_9,
+
+        //Letter keys
+        KEY_A,
+        KEY_B,
+        KEY_C,
+        KEY_D,
+        KEY_E,
+        KEY_F,
+        KEY_G,
+        KEY_H,
+        KEY_I,
+        KEY_J,
+        KEY_K,
+        KEY_L,
+        KEY_M,
+        KEY_N,
+        KEY_O,
+        KEY_P,
+        KEY_Q,
+        KEY_R,
+        KEY_S,
+        KEY_T,
+        KEY_U,
+        KEY_V,
+        KEY_W,
+        KEY_X,
+        KEY_Y,
+        KEY_Z,
+
+        //Arrow keys
+        LEFTARROW,
+        RIGHTARROW,
+        UPARROW,
+        DOWNARROW,
+
+        //Function keys
+        KEY_F1,
+        KEY_F2,
+        KEY_F3,
+        KEY_F4,
+        KEY_F5,
+        KEY_F6,
+        KEY_F7,
+        KEY_F8,
+        KEY_F9,
+        KEY_F10,
+        KEY_F11,
+        KEY_F12,
 
         //Keypad
         KP_SLASH,
@@ -26,7 +86,7 @@ namespace CSGO_CfgGen.Commands
         KP_HOME,
         KP_UPARROW,
         KP_PGUP,
-        KP_INS
+        KP_INS,
 
     }
 }
